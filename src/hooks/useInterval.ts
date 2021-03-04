f429de513a33542eb4f69530bc4f1f575c142d2e
forsnake13 <forsoapfc@gmail.com>
2021-03-04 11:36:16 +0800

    Useful useInterval hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export default function useInterval(callback, delay: number) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
