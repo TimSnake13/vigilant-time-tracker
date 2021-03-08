79c32abd9e8d1fcf5f4dd42ce396a3ece5b360a5
forsnake13 <forsoapfc@gmail.com>
2021-03-08 21:04:13 +0800

    Apollo client added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import "../styles/globals.css";
 
 import store from "../src/redux/store";
 import { Provider } from "react-redux";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Provider>
   );
 }
---
+++ pages/apollo.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+
+const GET_TITLE = gql`
+  query GetTitles {
+    books {
+      title
+      author
+    }
+  }
+`;
+
+const apollo = () => {
+  console.log(process.env.SERVER_ADDRESS);
+  const { loading, error, data } = useQuery(GET_TITLE);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
+  return (
+    <div>
+      Apollo Client Test:
+      {data.books.map((book) => (
+        <div key={book.title}>
+          Book: {book.title} {book.author}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default apollo;
